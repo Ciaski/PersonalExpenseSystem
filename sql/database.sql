ddc41888d45c61f737678a4f448a0a14436ce2f7
Ciaski <simonetagliaferri66@gmail.com>
2026-04-14 15:07:09 +0200

    database.sql

--- sql/database.sql
+++ sql/database.sql
@@ -22,8 +22,8 @@ CREATE TABLE IF NOT EXISTS budget_mensile (
 INSERT INTO categorie (nome) VALUES ('Alimentari');
 INSERT INTO categorie (nome) VALUES ('Trasporti');
 INSERT INTO categorie (nome) VALUES ('Svago');
-INSERT INTO spese (data_spesa, cifra, nota, cat_id) VALUES ('2025-05-01', 55.40, 'Spesa supermercato', 1);
-INSERT INTO spese (data_spesa, cifra, nota, cat_id) VALUES ('2025-05-02', 20.00, 'Ricarica abbonamento bus', 2);
-INSERT INTO spese (data_spesa, cifra, nota, cat_id) VALUES ('2025-05-05', 15.50, 'Pizza con amici', 3);
-INSERT INTO budget_mensile (mese_rif, cat_id, limite_max) VALUES ('2025-05', 1, 300.00);
-INSERT INTO budget_mensile (mese_rif, cat_id, limite_max) VALUES ('2025-05', 2, 50.00);
+INSERT INTO spese (data_spesa, cifra, nota, cat_id) VALUES ('2026-04-01', 55.40, 'Spesa supermercato', 1);
+INSERT INTO spese (data_spesa, cifra, nota, cat_id) VALUES ('2026-04-02', 20.00, 'Ricarica abbonamento bus', 2);
+INSERT INTO spese (data_spesa, cifra, nota, cat_id) VALUES ('2026-04-05', 15.50, 'Pizza con amici', 3);
+INSERT INTO budget_mensile (mese_rif, cat_id, limite_max) VALUES ('2026-04', 1, 300.00);
+INSERT INTO budget_mensile (mese_rif, cat_id, limite_max) VALUES ('2026-04', 2, 50.00);
